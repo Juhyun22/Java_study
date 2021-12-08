9534ead25f8241f3560f8985767e52f4109cf087
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2021-12-08 23:42:34 +0900

    Create quiz03.java

---
+++ JAVA1/1. variable_and_datatype/quiz03.java
@@ -0,0 +1,25 @@
+package Testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// 1. 평균 구하기 
+		// 반올림 잊지 말기!
+		int kor = 93;
+		int math = 88;
+		int eng = 94;
+		double avg = (kor + math + eng) / 3.0;
+		
+		System.out.println("국어 " + kor + "점, 수학 " + math + "점, 영어 " + eng + "점");
+		System.out.println("평균: " + Math.round(avg * 100) / (double)100);
+		
+		// 2. 화씨 구하기
+		// 소수점 사용되는지 잘 확인
+		double fah; // 화씨
+		double cel = 30.0; // 섭씨  
+		
+		fah = 9 / (double)5 * cel + 32;
+		System.out.println("섭씨 " + cel +"도는 화씨 " + fah + "도 입니다.");
+	}
+
+}
