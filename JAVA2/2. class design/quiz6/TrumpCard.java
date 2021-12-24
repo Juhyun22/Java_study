2c060476e4b986312c0e36ad0123d54cc39ca991
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2021-12-25 03:09:56 +0900

    Create TrumpCard.java

---
+++ JAVA2/2. class design/quiz6/TrumpCard.java
@@ -0,0 +1,21 @@
+package Testing;
+
+public class TrumpCard {
+	// field
+	private String shape;
+	private int number;
+	
+	public TrumpCard(String shape, int number) {
+		this.shape = shape;
+		this.number = number;
+	}
+	
+	public String getShape() {
+		return this.shape;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+	
+}
